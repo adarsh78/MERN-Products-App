697824871fcb5e28a2d7468a0445b6e7c12196ed
Adarsh k <you@example.com>
2024-09-16 01:09:55 +0530

    CRUD Completed with UI

---
+++ backend/models/product.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+    price : {
+        type: String,
+        required: true,
+    },
+    image : {
+        type: String,
+        required: true,
+    }, 
+},
+{
+    timestamps: true    // createdAt, updatedAt
+}
+);
+
+const Product = mongoose.model("Product", productSchema);    // mongoose will take "Product" and convert it to "products"
+
+export default Product;
